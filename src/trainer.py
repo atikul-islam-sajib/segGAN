57a3aac686939cd985558b3b61159a793c1cf690
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-05-20 14:58:41 +0200

    L1, L2 & ElasticNet loss is addded in src

--- src/trainer.py
+++ src/trainer.py
@@ -4,6 +4,8 @@ from torch.optim.lr_scheduler import StepLR
 
 from .helper import helpers
 from .utils import weight_init, device_init
+from .generator import Generator
+from .discriminator import Discriminator
 
 
 class Trainer:
@@ -82,11 +84,27 @@ class Trainer:
                 optimizer=self.optimizerD, step_size=self.step_size, gamma=self.gamma
             )
 
+    def l1_loss(self, model):
+        if isinstance(model, Generator):
+            return (torch.norm(params, 1) for params in model.parameters()).mean()
+        else:
+            raise ValueError("The model is not a Generator".capitalize())
+
     def l2_loss(self, model):
-        pass
+        if isinstance(model, Generator):
+            return (torch.norm(params, 2) for params in model.parameters()).mean()
+        else:
+            raise ValueError("The model is not a Generator".capitalize())
 
     def elastic_loss(self, model):
-        pass
+        if isinstance(model, Generator):
+            l1 = self.l1_loss(model=model)
+            l2 = self.l2_loss(model=model)
+
+            return l1 + l2
+
+        else:
+            raise ValueError("The model is not a Generator".capitalize())
 
     def saved_model_checkpoints(self, **kwargs):
         pass
@@ -108,4 +126,80 @@ class Trainer:
 
 
 if __name__ == "__main__":
-    trainer = Trainer()
+    parser = argparse.ArgumentParser(description="Traine code for seGAN".title())
+    parser.add_argument(
+        "--channels",
+        type=int,
+        default=3,
+        help="Define the number of channels".capitalize(),
+    )
+    parser.add_argument(
+        "--lr", type=float, default=0.0002, help="Define the learning rate".capitalize()
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=100,
+        help="Define the number of epochs".capitalize(),
+    )
+    parser.add_argument(
+        "--adam", type=bool, default=True, help="Define the optimizer".capitalize()
+    )
+    parser.add_argument(
+        "--SGD", type=bool, default=False, help="Define the optimizer".capitalize()
+    )
+    parser.add_argument(
+        "--device", type=str, default="cuda", help="Define the device".capitalize()
+    )
+    parser.add_argument(
+        "--beta1", type=float, default=0.5, help="Define the beta1".capitalize()
+    )
+    parser.add_argument(
+        "--beta2", type=float, default=0.999, help="Define the beta2".capitalize()
+    )
+    parser.add_argument(
+        "--momentum", type=float, default=0.90, help="Define the momentum".capitalize()
+    )
+    parser.add_argument(
+        "--smooth", type=float, default=0.001, help="Define the smooth".capitalize()
+    )
+    parser.add_argument(
+        "--step_size", type=int, default=10, help="Define the step size".capitalize()
+    )
+    parser.add_argument(
+        "--gamma", type=float, default=0.5, help="Define the gamma".capitalize()
+    )
+    parser.add_argument(
+        "--lr_scheduler",
+        type=bool,
+        default=False,
+        help="Define the lr scheduler".capitalize(),
+    )
+    parser.add_argument(
+        "--is_display", type=bool, default=True, help="Define the display".capitalize()
+    )
+    parser.add_argument(
+        "--is_weight_init",
+        type=bool,
+        default=True,
+        help="Define the weight init".capitalize(),
+    )
+    args = parser.parse_args()
+
+    trainer = Trainer(
+        channels=args.channels,
+        lr=args.lr,
+        epochs=args.epochs,
+        adam=args.adam,
+        SGD=args.SGD,
+        device=args.device,
+        beta1=args.beta1,
+        beta2=args.beta2,
+        momentum=args.momentum,
+        smooth=args.smooth,
+        step_size=args.step_size,
+        gamma=args.gamma,
+        lr_scheduler=args.lr_scheduler,
+        is_display=args.is_display,
+        is_weight_init=args.is_weight_init,
+    )
