9338ee3c933dca8116b1d421c15f3b18c650483c
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-05-20 14:29:24 +0200

    weight init is updated in src

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import os
 import yaml
 import torch
 import joblib
+import torch.nn as nn
 import traceback
 
 
@@ -51,3 +52,14 @@ def device_init(device="cuda"):
 
     else:
         return torch.device("cpu")
+
+
+def weight_init(m):
+    classname = m.__class__.__name__
+
+    if classname.find("Conv") != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+
+    elif classname.find("BatchNorm") != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
