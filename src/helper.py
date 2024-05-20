9c88b931ab33cd8f2f02da1c4d366e9a7c787b87
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-05-20 14:12:48 +0200

    dice loss is added in the helpers method

--- src/helper.py
+++ src/helper.py
@@ -3,6 +3,7 @@ import torch.optim as optim
 
 from .utils import config, validate_path, load
 from .l1_loss import L1Loss
+from .dice_loss import DiceLoss
 from .generator import Generator
 from .discriminator import Discriminator
 
@@ -28,6 +29,7 @@ def helpers(**kwargs):
     beta1 = kwargs["beta1"]
     beta2 = kwargs["beta2"]
     momentum = kwargs["momentum"]
+    smooth = kwargs["smooth"]
 
     netG = Generator(in_channels=channels)
     netD = Discriminator(in_channels=channels)
@@ -41,6 +43,7 @@ def helpers(**kwargs):
         optimizerD = optim.SGD(params=netD.parameters(), lr=lr, momentum=momentum)
 
     l1loss = L1Loss(reduction="mean")
+    diceloss = DiceLoss(smooth=smooth)
     dataloader = load_dataloader()
 
     return {
@@ -49,6 +52,7 @@ def helpers(**kwargs):
         "optimizerG": optimizerG,
         "optimizerD": optimizerD,
         "l1loss": l1loss,
+        "diceloss": diceloss,
         "train_dataloader": dataloader["train_dataloader"],
         "test_dataloader": dataloader["test_dataloader"],
     }
@@ -58,6 +62,7 @@ if __name__ == "__main__":
     beta1 = config()["model"]["beta1"]
     beta2 = config()["model"]["beta2"]
     momentum = config()["model"]["momentum"]
+    smooth = config()["model"]["dice_smooth"]
 
     init = helpers(
         channels=3,
@@ -67,6 +72,7 @@ if __name__ == "__main__":
         beta1=beta1,
         beta2=beta2,
         momentum=momentum,
+        smooth=smooth,
     )
 
     print(init["netG"])
@@ -74,5 +80,6 @@ if __name__ == "__main__":
     print(init["optimizerG"])
     print(init["optimizerD"])
     print(init["l1loss"])
+    print(init["diceloss"])
     print(init["train_dataloader"])
     print(init["test_dataloader"])
